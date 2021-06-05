053b15b30e6ee9252d9ade474f595444a8562d24
Zeeshan Ali <zali2592@gmail.com>
2021-06-05 17:56:00 +0530

    adding intro page

---
+++ src/components/Fire.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Fire.css";
+function Fire() {
+  return (
+    <div className="fire">
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="gooey">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+            <feColorMatrix
+              in="blur"
+              type="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              result="highContrastGraphic"
+            />
+            <feComposite
+              in="SourceGraphic"
+              in2="highContrastGraphic"
+              operator="atop"
+            />
+          </filter>
+        </defs>
+      </svg>
+
+      <button id="gooey-button">
+        View My Work
+        <span class="bubbles">
+          <span class="bubble"></span>
+          <span class="bubble"></span>
+          <span class="bubble"></span>
+          <span class="bubble"></span>
+          <span class="bubble"></span>
+          <span class="bubble"></span>
+          <span class="bubble"></span>
+          <span class="bubble"></span>
+          <span class="bubble"></span>
+          <span class="bubble"></span>
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default Fire;
